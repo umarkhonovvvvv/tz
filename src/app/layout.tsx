fd879254e21a016a351b6aa6e1a4f85cff32d4a8
hp <hp@DESKTOP-7BTHN8F>
2026-04-15 23:10:49 +0500

    Fix: Add use client directive to layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,12 @@
-﻿import "./globals.css";
+﻿"use client"
+import "./globals.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { useState, ReactNode } from "react";
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   const [queryClient] = useState(() => new QueryClient());
   return (
-    <html lang="en" className="dark">
+    <html lang="uk" className="dark">
       <body className="bg-black text-white antialiased">
         <QueryClientProvider client={queryClient}>
           {children}
